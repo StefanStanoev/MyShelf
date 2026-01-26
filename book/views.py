78aef3b8181d45a9527dd353ef5881b0bb6e26ba
StefanStanoev <stefansta999@gmail.com>
2026-01-26 17:18:11 +0200

    Initial commit

--- book/views.py
+++ book/views.py
@@ -1,29 +1,3 @@
-import datetime
-from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView, UpdateView, DetailView, DeleteView
 
-from book.forms import BookForm
-from book.models import Book
-
-
-class BookCreateView(CreateView):
-    template_name = 'book/add_book.html'
-    model = Book
-    form_class = BookForm
-    success_url = '/list_of_books/' 
-    permission_required = 'book.add_book'
-
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        now = datetime.datetime.now()
-        context['current_datetime'] = now
-
-        active_books = Book.objects.filter(active=True)
-        context['active_books'] = active_books
-
-        return context
-    
-    
+# Create your views here.
